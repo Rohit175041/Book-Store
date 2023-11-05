c4c2711b71a8ddb5b5b503f899f2bbd43fafdfec
Rohit175041 <rohit175041@gmail.com>
2023-11-05 20:44:39 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -11,21 +11,14 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
-app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+// app.use(cors());
 
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
+// routes
 app.use('/books', booksRoute);
 
 mongoose
